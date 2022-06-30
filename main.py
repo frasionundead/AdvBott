a91dc5cce1f0821ff12b6b978cde7bebf9081e49
frasionundead <gays.foxy@gmail.com>
2022-06-30 13:59:31 +0300

    Auto build APK 32bit and 64bit files

--- main.py
+++ main.py
@@ -1,54 +1,124 @@
 from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, ListProperty
 
 from kivymd.app import MDApp
+from kivymd.theming import ThemableBehavior
+from kivymd.uix.list import OneLineIconListItem, MDList
+
+KV = '''
+# Menu item in the DrawerList list.
+<ItemDrawer>:
+    theme_text_color: "Custom"
+    on_release: self.parent.set_color_item(self)
+
+    IconLeftWidget:
+        id: icon
+        icon: root.icon
+        theme_text_color: "Custom"
+        text_color: root.text_color
 
 
-class Test(MDApp):
+<ContentNavigationDrawer>:
+    orientation: "vertical"
+    padding: "8dp"
+    spacing: "8dp"
 
-    def build(self):
-        self.theme_cls.material_style = "M3"
-        return Builder.load_string(
-            '''
-#:import get_color_from_hex kivy.utils.get_color_from_hex
+    AnchorLayout:
+        anchor_x: "left"
+        size_hint_y: None
+        height: avatar.height
+
+        Image:
+            id: avatar
+            size_hint: None, None
+            size: "56dp", "56dp"
+            source: "data/logo/kivy-icon-256.png"
+
+    MDLabel:
+        text: "Frasion"
+        font_style: "Button"
+        size_hint_y: None
+        height: self.texture_size[1]
+
+    MDLabel:
+        text: "gays.foxy@gmail.com"
+        font_style: "Caption"
+        size_hint_y: None
+        height: self.texture_size[1]
+
+    ScrollView:
+
+        DrawerList:
+            id: md_list
 
 
-MDScreen:
 
-    MDBottomNavigation:
-        panel_color: get_color_from_hex("#eeeaea")
-        selected_color_background: get_color_from_hex("#97ecf8")
-        text_color_active: 0, 0, 0, 1
+Screen:
 
-        MDBottomNavigationItem:
-            name: 'screen 1'
-            text: 'Mail'
-            icon: 'gmail'
-            badge_icon: "numeric-10"
+    MDNavigationLayout:
 
-            MDLabel:
-                text: 'Mail'
-                halign: 'center'
+        ScreenManager:
 
-        MDBottomNavigationItem:
-            name: 'screen 2'
-            text: 'Discord'
-            icon: 'discord'
-            badge_icon: "numeric-5"
+            Screen:
 
-            MDLabel:
-                text: 'Discord'
-                halign: 'center'
+                BoxLayout:
+                    orientation: 'vertical'
 
-        MDBottomNavigationItem:
-            name: 'screen 3'
-            text: 'LinkedIN'
-            icon: 'linkedin'
+                    MDToolbar:
+                        title: "AdvertisementBotA"
+                        elevation: 10
+                        left_action_items: [['menu', lambda x: nav_drawer.set_state("open")]]
 
-            MDLabel:
-                text: 'LinkedIN'
-                halign: 'center'
+                    Widget:
+
+
+        MDNavigationDrawer:
+            id: nav_drawer
+
+            ContentNavigationDrawer:
+                id: content_drawer
 '''
-        )
 
 
-Test().run()
+class ContentNavigationDrawer(BoxLayout):
+    pass
+
+
+class ItemDrawer(OneLineIconListItem):
+    icon = StringProperty()
+    text_color = ListProperty((0, 0, 0, 1))
+
+
+class DrawerList(ThemableBehavior, MDList):
+    def set_color_item(self, instance_item):
+        """Called when tap on a menu item."""
+
+        # Set the color of the icon and text for the menu item.
+        for item in self.children:
+            if item.text_color == self.theme_cls.primary_color:
+                item.text_color = self.theme_cls.text_color
+                break
+        instance_item.text_color = self.theme_cls.primary_color
+
+
+class AdvertisementBotApp(MDApp):
+    def build(self):
+        return Builder.load_string(KV)
+
+    def on_start(self):
+        icons_item = {
+            "folder": "My files",
+            "account-multiple": "Shared with me",
+            "star": "Starred",
+            "history": "Recent",
+            "checkbox-marked": "Shared with me",
+            "upload": "Upload",
+        }
+        for icon_name in icons_item.keys():
+            self.root.ids.content_drawer.ids.md_list.add_widget(
+                ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+            )
+
+
+AdvertisementBotApp().run()
